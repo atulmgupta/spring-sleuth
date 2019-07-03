618e455953ed54e188ee997e1bb562d9e4ec6b2c
Atul <atulmohan@lenderprice.com>
2019-07-03 16:07:11 -0700

    Added initial projects

---
+++ spring-zipkin-server/src/main/java/com/atul/springzipkinserver/SpringZipkinServerApplication.java
@@ -0,0 +1,13 @@
+package com.atul.springzipkinserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringZipkinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringZipkinServerApplication.class, args);
+	}
+
+}
